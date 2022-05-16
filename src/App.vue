d65e6af1b82112e05b7162e91118fec1a0ac8534
Ftm-amani <ftm.amani95@gmail.com>
2022-05-16 10:16:18 +0430

    implement add and delete function

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div className="container">
+    <app-new-quote @quoteAdded = "newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid";
+import NewQuote from "./components/NewQuote";
+    export default {
+        data:function (){
+          return {
+            quotes: [
+              'just to see something'
+            ],
+            maxQuotes: 10
+
+          }
+        },
+      methods: {
+          newQuote(quote) {
+            this.quotes.push(quote)
+          },
+        deleteQuote(index){
+            this.quotes.splice(index, 1 )
+        }
+      },
+      components: {
+          appQuoteGrid : QuoteGrid,
+          appNewQuote : NewQuote
+      }
+    }
+</script>
+
+<style>
+</style>
